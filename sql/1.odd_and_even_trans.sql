a83e86dec3bb71e23c4debcaa04c85a9b8461d40
thethien8a <thethien1708@gmail.com>
2026-03-04 11:30:03 +0700

    done 1 sql

--- sql/1.odd_and_even_trans.sql
+++ sql/1.odd_and_even_trans.sql
@@ -2,3 +2,30 @@
 https://leetcode.com/problems/odd-and-even-transactions/
 */
 
+-- WITH TEMP_TABLE AS (
+--     SELECT
+--         transaction_date,
+--         SUM(amount) FILTER(WHERE amount % 2 = 1) AS odd_sum,
+--         SUM(amount) FILTER(WHERE amount % 2 = 0) AS even_sum
+--     FROM
+--         transactions
+--     GROUP BY
+--         transaction_date
+--     ORDER BY
+--         transaction_date ASC
+-- )
+-- SELECT
+--     transaction_date,
+--     CASE WHEN odd_sum IS NULL THEN 0 ELSE odd_sum END,
+--     CASE WHEN even_sum IS NULL THEN 0 ELSE even_sum END
+-- FROM
+--     TEMP_TABLE
+
+
+-- more optimize
+select transaction_date, 
+sum(case when amount % 2 != 0 then amount else 0 end) as odd_sum, 
+sum(case when amount % 2 = 0 then amount else 0 end) as even_sum
+from transactions 
+group by transaction_date
+order by transaction_date;
