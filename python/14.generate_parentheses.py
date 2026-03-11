a1614a611e7fc3f5749f7211cd608dee2393f57c
thethien8a <thethien1708@gmail.com>
2026-03-11 11:49:50 +0700

    Done 14: backtrack algorithm

---
+++ python/14.generate_parentheses.py
@@ -0,0 +1,24 @@
+""" 
+https://leetcode.com/problems/generate-parentheses/
+"""
+
+from aiohttp_retry import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+        def backtrack(current: str, open_count: int, close_count: int):
+            if len(current) == 2 * n:
+                result.append(current)
+                return
+            
+            if open_count < n:
+                backtrack(current + "(", open_count + 1, close_count)
+            
+            if close_count < open_count:
+                backtrack(current + ")", open_count, close_count + 1)
+
+        backtrack("", 0, 0)
+        return result
