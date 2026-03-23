d9c73f02d4dd7784b8de905cb0886572790d3a0b
thethien8a <thethien1708@gmail.com>
2026-03-24 00:10:10 +0700

    done python 36

---
+++ python/26.permutation_2.py
@@ -0,0 +1,39 @@
+"""
+https://leetcode.com/problems/permutations-ii/
+"""
+
+from typing import List
+
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        used = [False] * len(nums)
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack(path)
+
+                path.pop()
+                used[i] = False
+
+        backtrack([])
+        return res
+
+
+            
+        
